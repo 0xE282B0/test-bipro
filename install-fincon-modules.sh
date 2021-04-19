97e1e24967946d54181a07b5f9568c075bb89676
Priyanka Goel <priyankagoelskd@gmail.com>
2021-04-19 16:27:29 +0200

    Added Logs

--- install-fincon-modules.sh
+++ install-fincon-modules.sh
@@ -3,7 +3,9 @@
 deliveryDirectory="$(dirname "$BASH_SOURCE")"
 
 # Install FINCON modules
+
 cd ${deliveryDirectory}
+echo ${deliveryDirectory}
 
 mvn org.apache.maven.plugins:maven-install-plugin:2.5.2:install-file -Dfile="./fincon-libs/bipro-adapter-parent-3.4.3.1-SNAPSHOT.pom"          -DpomFile="./fincon-libs/bipro-adapter-parent-3.4.3.1-SNAPSHOT.pom"
 mvn org.apache.maven.plugins:maven-install-plugin:2.5.2:install-file -Dfile="./fincon-libs/generic-api-3.4.3.1-SNAPSHOT.jar"                   -DpomFile="./fincon-libs/generic-api-3.4.3.1-SNAPSHOT.pom"                     -Djavadoc="./fincon-libs/generic-api-3.4.3.1-SNAPSHOT-javadoc.jar"
